ca45fc2489a404dc4af7b001228f69ced25b86e0
Sherlock-exe <87928958+sherlock-exe@users.noreply.github.com>
2021-08-18 20:41:53 +0300

    asdasdas

--- texera/plugins/afk.py
+++ texera/plugins/afk.py
@@ -26,7 +26,8 @@ async def on_tag(client:Client, message:Message):
     msg = "Şu an AFK'yım!"
     mentioned = message.mentioned
     rep_m = message.reply_to_message
-    me = await client.get_me().id
+    me = await client.get_me()
+    me = me.id
     if mentioned or rep_m and rep_m.from_user and rep_m.from_user.id == me:
         if TEMP_AYAR["AFK"] != "0":
             if TEMP_AYAR["AFK"][+1:] == '':
