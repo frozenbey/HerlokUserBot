ea994055ee3b05aaa04ef8213556dc35ea5251e3
Sherlock-exe <87928958+sherlock-exe@users.noreply.github.com>
2021-08-27 16:57:34 +0300

    Update json.py

--- texera/plugins/json.py
+++ texera/plugins/json.py
@@ -1,11 +1,23 @@
 from texera.cmdhelp import CmdHelp
 from pyrogram import Client, filters
 from pyrogram.types import Message
+from pyrogram.errors import MessageTooLong
+import os
+
 
 @Client.on_message(filters.command(['json'], ['!','.','/']) & filters.me)
 async def jsn_ver(client:Client, message:Message):
     if message.reply_to_message:
-        await message.edit(f"```{message.reply_to_message}```")
+        try:
+            await message.edit(f"```{message.reply_to_message}```")
+        except MessageTooLong:
+            await message.edit("`Mesajın uzunluğu izin verilenden fazla. Dosya olarak gönderiyorum...`")
+            text = str(message.reply_to_message)
+            with open("json.txt", "w", encoding="utf-8") as dosya:
+                dosya.write(text)
+                await client.send_document("json.txt")
+            os.remove("json.txt")
+            
     else:
         await message.edit("`Lütfen bir mesajı yanıtlayın`")
 
