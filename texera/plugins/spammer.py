f3c967498c148fd62fb98d837f5d59ce738430a6
Sherlock-exe <87928958+sherlock-exe@users.noreply.github.com>
2021-08-28 19:54:19 +0300

    Update spammer.py

--- texera/plugins/spammer.py
+++ texera/plugins/spammer.py
@@ -2,20 +2,41 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 import asyncio
 from texera.cmdhelp import CmdHelp
+import asyncio
 
-@Client.on_message(filters.command(['spam'], ['!','.','/']) & filters.me)
-async def spam(client:Client, message:Message):
-    msg = message.text
-    msgsplit = msg.split(" ", 2)
+def ReplyCheck(message: Message):
+    reply_id = None
 
-    try:
-        cmd1 = msgsplit[1]
-        cmd2 = msgsplit[2]
-        await message.delete()
-    except:
-        return
-    chat_id = message.chat.id
-    for _ in range(0, int(cmd1)):
-        await client.send_message(chat_id, cmd2)
-        await asyncio.sleep(0.7)
+    if message.reply_to_message:
+        reply_id = message.reply_to_message.message_id
+
+    elif not message.from_user.is_self:
+        reply_id = message.message_id
+
+    return reply_id
+
+
+@Client.on_message(filters.command("spam", ".") & filters.me)
+async def spam(_, message: Message):
+    #--> Mesajı Sil
+    await message.delete()
+
+    times = message.command[1]
+    to_spam = " ".join(message.command[2:])
+    
+    #--> Grup spammer
+    if message.chat.type in ["supergroup", "group"]:
+        for _ in range(int(times)):
+            await UserBot.send_message(
+                message.chat.id, to_spam, reply_to_message_id=ReplyCheck(message)
+            )
+            await asyncio.sleep(0.20)
+
+    #--> Priv Spammer
+    if message.chat.type == "private":
+        for _ in range(int(times)):
+            await UserBot.send_message(message.chat.id, to_spam)
+            await asyncio.sleep(0.20)
+
+            
 CmdHelp("spammer").add_command("spam", "<sayı> <spamlanacak yazı>", "Spam yapar.").add_warning("Sorumluluk kabul etmiyoruz!").add()
